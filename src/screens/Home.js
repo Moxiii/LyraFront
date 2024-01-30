9fcb24ce1f5f67fb0a58f91406f986bf28b0c1dc
didine712 <aguenassia@gmail.com>
2024-01-30 15:12:18 +0100

    homepage ajustée

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
-import Header from "../components/Header";
+import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 
 export default function Home() {
   const backgroundImage = require("../../assets/img/GeorgesBG.jpeg");
+  const invisibleGeorgesImage = require("../../assets/img/georgesinvisible.png");
 
   if (!backgroundImage) {
     return <View />;
@@ -16,8 +16,18 @@ export default function Home() {
         resizeMode="cover"
         style={styles.container}
       >
-        <Header title="Georges" showConversationIcon={true} />
-        <Text>Home Screen</Text>
+        <View style={styles.georges}>
+          <Text style={styles.title}>Georges</Text>
+          <Text style={styles.subtitle}>
+            Connecte Georges à toutes tes applications. Et il fera le reste
+          </Text>
+        </View>
+        <View style={styles.invisibleGeorgesContainer}>
+          <Image
+            source={invisibleGeorgesImage}
+            style={styles.invisibleGeorgesImage}
+          />
+        </View>
       </ImageBackground>
     </View>
   );
@@ -28,5 +38,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  georges: {
+    flex: 1,
+    marginTop: "30%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "white",
+    textAlign: "center",
+    paddingHorizontal: 20,
+  },
+  invisibleGeorgesContainer: {
+    paddingBottom:"40%"
+  },
+  invisibleGeorgesImage: {
+    width: 400,
+    height: 300,
   },
 });
