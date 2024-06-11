fb2d933e17aaaf0a3d6f11cd3115a6fd4eb887bf
didine712 <aguenassia@gmail.com>
2024-06-11 09:39:02 +0200

    button changed

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -157,6 +157,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 28,
+    height: 95,
     borderRadius: 30,
     overflow: "hidden",
   },
